fcb77baf554a05161eff53ca04e71aea6728c16d
Evan Ho <evanho9@live.com>
2019-03-27 22:08:36 -0700

    Add css styling

--- loginform.php
+++ loginform.php
@@ -1,6 +1,7 @@
 <?php
   require_once 'login.php';
   require_once 'tools.php';
+  echo "<link rel='stylesheet' href='style.css'>";
   
   $conn = new mysqli($hn, $un, $pw, $db);
   if ($conn->connect_error) die ($conn->connect_error);
@@ -8,15 +9,21 @@
   if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = get_post($conn, 'username');
     $password = get_post($conn, 'password');
-    add_user($username, $password);
+    if (user_exists($username)) {
+      echo "Username '$username' exists in the database already.";
+    } else {
+      add_user($username, $password);
+    }
   }
   
   echo <<<_END
+  <div class="userform">
   <form action="loginform.php" method="post"><pre>
-  Username<input type="text" name="username">
-  Password<input type="text" name="password">
+  Username <input type="text" name="username"><br>
+  Password <input type="text" name="password"><br>
   <input type="submit" value="Submit">
   </pre></form>
+  </div>
 _END;
   
   //Print all the user names and passwords
--- tools.php
+++ tools.php
@@ -4,21 +4,33 @@
   function salt_and_hash($a) {
     $salt1 = "^m%1";
     $salt2 = "7u^w";
-    return hash('next123', $salt1, $a, $salt2);
+    return hash('ripemd128', $salt1 . $a . $salt2);
   }
   
   function get_post($conn, $var) {
     return $conn->real_escape_string($_POST[$var]);
   }
   
+  function user_exists($username) {
+    global $hn, $un, $pw, $db;
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if ($conn->connect_error) die ($conn->connect_error);
+    $query = "SELECT * FROM users WHERE username=''$username''";
+    $result = $conn->query($query);
+    if (!$result) return false;
+    return true;
+  }
+  
   function add_user($username, $password) {
     global $hn, $un, $pw, $db;
     $conn = new mysqli($hn, $un, $pw, $db);
     if ($conn->connect_error) die ($conn->connect_error);
+    
+    $password = salt_and_hash($password);
     $query = "INSERT INTO users VALUES" .
             "('$username','$password')";
     $result = $conn->query($query);
-    if (!$result) echo "INSERT failed: $query<br>" . $conn->error . "<br><br>";
+    if (!$result) echo "INSERT failed. <br>" . $conn->error . "<br>";
     
   }
 ?>
