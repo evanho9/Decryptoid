084940fa899a381e59ca738a2b9467f06f06eb5b
Evan Ho <evanho9@live.com>
2019-04-05 17:18:43 -0700

    Implement simple substitution cipher

--- cryptotools.php
+++ cryptotools.php
@@ -1,26 +1,44 @@
 <?php
 
-  function substitution_encrypt() {
+  $test_alphabet = array("a" => "x", "b" => "y", "c" => "z");
+
+  echo substitution_encrypt("abc", $test_alphabet) . "<br>";
+  
+  echo substitution_decrypt("xyz", $test_alphabet);
+
+  function substitution_encrypt($to_encrypt, $alphabet) {
+    $to_encrypt = str_split($to_encrypt);
+    $res = str_repeat("*", sizeOf($to_encrypt));
+    for ($i=0; $i<sizeOf($to_encrypt); $i++) {
+      if ($res[$i] !== " ")
+        $res[$i] = $alphabet[$to_encrypt[$i]];
+    }
+    return $res;
+  }
+  
+  function substitution_decrypt($to_decrypt, $alphabet) {
+    $to_decrypt = str_split($to_decrypt);
+    $res = str_repeat("*", sizeOf($to_decrypt));
+    for ($i=0; $i<sizeOf($to_decrypt); $i++) {
+      if ($res[$i] !== " ")
+        $res[$i] = array_search($to_decrypt[$i], $alphabet);
+    }
+    return $res;
+  }
+  
+  function double_transposition_encrypt($to_encrypt, $alphabet) {
     
   }
   
-  function substitution_decrypt($key) {
+  function double_transposition_decrypt($to_decrypt, $alphabet) {
     
   }
   
-  function double_transposition_encrypt() {
+  function RC4_encrypt($to_encrypt, $alphabet) {
     
   }
   
-  function double_transposition_decrypt($key) {
-    
-  }
-  
-  function RC4_encrypt() {
-    
-  }
-  
-  function RC4_decrypt($key) {
+  function RC4_decrypt($to_decrypt, $alphabet) {
     
   }
   
