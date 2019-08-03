b85d8ed4eb2538247f035589130f807e588f72c1
Evan Ho <evanho9@live.com>
2019-08-03 01:50:34 -0700

    Fix server config

--- php/login.php
+++ php/login.php
@@ -1,6 +1,6 @@
 <?php
-  $hn = 'localhost';
-  $un = 'root';
-  $pw = '';
-  $db = 'decryptoid';
+  $hn = 'us-cdbr-iron-east-02.cleardb.net';
+  $un = 'b31dc71502c9c7';
+  $pw = '316d7099';
+  $db = 'heroku_37a6d4df2981199';
 ?>
